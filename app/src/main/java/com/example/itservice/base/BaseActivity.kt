00f0294aef91003eee8ea07c3a167385650b6d59
Mustofa Mahmud <mooncseru14@gmail.com>
2023-02-06 23:07:27 +0600

    activities are made portrait

--- app/src/main/java/com/example/itservice/base/BaseActivity.kt
+++ app/src/main/java/com/example/itservice/base/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.itservice.base
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
